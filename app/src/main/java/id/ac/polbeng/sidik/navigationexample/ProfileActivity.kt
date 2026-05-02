2f4dd64f19da34c9c9bb30e75595f7fc098a86ee
sidiq02 <emailsidiq02@gmail.com>
2026-05-02 23:33:43 +0700

    first commit

---
+++ app/src/main/java/id/ac/polbeng/sidik/navigationexample/ProfileActivity.kt
@@ -0,0 +1,17 @@
+package id.ac.polbeng.sidik.navigationexample
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import id.ac.polbeng.sidik.navigationexample.databinding.ActivityProfileBinding
+
+class ProfileActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityProfileBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityProfileBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+}
