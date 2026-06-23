437a59b71b2490a90d598924bcc527b175c84d03
sidiq02 <emailsidiq02@gmail.com>
2026-06-24 00:52:49 +0700

    first commit

--- app/src/main/java/id/ac/polbeng/sidik/navigationexample/DetailCategoryFragment.kt
+++ app/src/main/java/id/ac/polbeng/sidik/navigationexample/DetailCategoryFragment.kt
@@ -5,52 +5,52 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.navigation.findNavController
-import id.ac.polbeng.sidik.navigationexample.databinding.FragmentCategoryBinding
-
+import androidx.navigation.Navigation
+import id.ac.polbeng.sidik.navigationexample.databinding.FragmentDetailCategoryBinding
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-
+private const val ARG_PARAM1 = "param1"
+private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
- * Use the [CategoryFragment.newInstance] factory method to
+ * Use the [DetailCategoryFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
-class CategoryFragment : Fragment() {
+class DetailCategoryFragment : Fragment() {
     // TODO: Rename and change types of parameters
-    private var _binding: FragmentCategoryBinding? = null
+    private var _binding: FragmentDetailCategoryBinding? = null
     private val binding get() = _binding!!
 
-    companion object {
-        val EXTRA_NAME = "extra_name"
-        val EXTRA_STOCK = "extra_stock"
-    }
+
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View {
+    ): View? {
         // Inflate the layout for this fragment
-        _binding = FragmentCategoryBinding.inflate(inflater, container,false)
+        _binding = FragmentDetailCategoryBinding.inflate(inflater, container, false)
         val view = binding.root
         return view
-
     }
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        binding.btnCategoryLifestyle.setOnClickListener { view ->
-            val mBundle = Bundle()
-            mBundle.putString(EXTRA_NAME, "Lifestyle")
-            mBundle.putLong(EXTRA_STOCK, 7)
+        val dataName = arguments?.getString(CategoryFragment.EXTRA_NAME)
+        val dataDescription =
+            arguments?.getLong(CategoryFragment.EXTRA_STOCK)
 
-            view.findNavController().navigate(R.id.action_categoryFragment_to_detailCategoryFragment, mBundle)
-        }
+        binding.tvCategoryName.text = dataName
+        binding.tvCategoryDescription.text = "Stock : $dataDescription"
+        binding.btnHome.setOnClickListener(Navigation.createNavigateOnClickListener(R.id.action_detailCategoryFragment_to_homeFragment))
+
     }
 
     override fun onDestroy() {
         super.onDestroy()
         _binding = null
     }
+
 }
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,18 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
+buildscript {
+    repositories {
+        google()
+        mavenCentral()
+    }
+    dependencies {
+        classpath("androidx.navigation:navigation-safe-args-gradle-plugin:2.8.9")
+
+
+    }
+
+}
 plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.kotlin.android) apply false
+    id("androidx.navigation.safeargs.kotlin") version "2.8.9" apply false
 }
